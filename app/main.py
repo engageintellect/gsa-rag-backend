f61a4954692d4090ff0a62540ad2d30a87c98ff9
engageintellect <jc9361@gmail.com>
2024-02-10 00:33:12 -0800

    init change

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import boto3
 from langchain import FewShotPromptTemplate, LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.llms.bedrock import Bedrock
 from langchain_community.document_loaders import UnstructuredHTMLLoader
+import uvicorn
+import logging.config
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Set this to the appropriate origins if needed
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 class Question(BaseModel):
     user_question: str
@@ -20,6 +33,7 @@ sts_client = boto3.client('sts', region_name=region_name)
 # Initialize Bedrock client globally
 def initialize_bedrock_client():
     response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=session_name)
+
     credentials = response['Credentials']
     return boto3.Session(
         aws_access_key_id=credentials['AccessKeyId'],
@@ -31,13 +45,23 @@ def initialize_bedrock_client():
 bedrock_runtime = initialize_bedrock_client()
 
 # List of document paths
-document_paths = [
-    "/home/ubuntu/gsa-rag/docs/0VV35P.3RLG4G_47QTCA20D00B2_INDUCTIVEHEALTH123020.html",
-    "/home/ubuntu/gsa-rag/docs/0WG872.3S6L5T_47QTCA20D006Y_TMETRICSIFSSPRICELISTREV.html",
-    "/home/ubuntu/gsa-rag/docs/0WTKWJ.3SJXV8_47QTCA20D00EY_47QTCA20D00EY.html"
+
+document_path = "/home/ubuntu/gsa-rag/docs/"
+document_names = [
+    "0VV35P.3RLG4G_47QTCA20D00B2_INDUCTIVEHEALTH123020.html",
+    "0WG872.3S6L5T_47QTCA20D006Y_TMETRICSIFSSPRICELISTREV.html",
+    "0WTKWJ.3SJXV8_47QTCA20D00EY_47QTCA20D00EY.html",
+    "0VVTSA.3RM6R1_GS-35F-148DA_GSAPRECISEDIGITALGS35F148DAIFSS6002021REV.html",
 ]
 
-# Load documents and concatenate their contents
+document_paths = []
+for document_name in document_names:
+    document_paths.append(document_path + document_name)
+
+print("DOCS:")
+for x in document_paths:
+    print(x)
+
 fulltext = ""
 for path in document_paths:
     loader = UnstructuredHTMLLoader(path)
@@ -53,34 +77,73 @@ model_id = 'anthropic.claude-v2:1'
 # Hello World endpoint
 @app.get("/hello")
 async def read_root():
-    return {"message": "Hello, World!"}
+    uvicorn_logger.info("Hello endpoint was accessed.")
+    return {"message": "Hello, from Resonant Logic!"}
 
 @app.post("/generate_answer/")
 async def generate_answer(question: Question):
-    # Q&A prompt template
-    user_question = question.user_question
-    question_template = """
-    You are a helpful assistant. The {doc_text} contains quotes from 4 companies.
-    Answer the following questions only on provided info.
-    List all company names.
-    """
+    try:
+        uvicorn_logger.info("Received question: %s", question.user_question)
+        user_question = question.user_question
+        question_template = """
+        You are a helpful assistant. Read the document inside <doc> tags that contains proposals for products and services from 4 vendors.
+        <doc>
+        {doc_text}
+        </doc>
+        """
 
-    question_template = question_template + user_question
-    prompt = PromptTemplate(template=question_template, input_variables=[""])
+        question_template = question_template + user_question + "Think step by Step"
+        prompt = PromptTemplate(template=question_template, input_variables=[""])
 
-    # Initialize LLM chain and run
-    bedrock_llm = Bedrock(
-        model_id=model_id,
-        client=bedrock_runtime,
-        model_kwargs={
-            'max_tokens_to_sample': 4096,
-            'temperature': 0.0,
-            'top_k': 250,
-            'top_p': 0.999
+        # Initialize LLM chain and run
+        bedrock_llm = Bedrock(
+            model_id=model_id,
+            client=bedrock_runtime,
+            model_kwargs={
+                'max_tokens_to_sample': 4096,
+                'temperature': 1.0,
+                'top_k': 250,
+                'top_p': 0.999
+            }
+        )
+
+        llm_chain = LLMChain(prompt=prompt, llm=bedrock_llm)
+        output = llm_chain.run({"doc_text": fulltext})
+
+        return output
+    except Exception as e:
+        uvicorn_logger.exception("An error occurred while generating answer: %s", str(e))
+        return {"error": "An error occurred"}
+
+# Configure UVicorn logging
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         }
-    )
+    },
+    "handlers": {
+        "file": {
+            "class": "logging.FileHandler",
+            "formatter": "default",
+            "filename": "/home/ubuntu/gsa-rag/logs/uvicorn.log",
+        },
+        "stream": {
+            "class": "logging.StreamHandler",
+            "formatter": "default"
+        }
+    },
+    "loggers": {
+        "uvicorn": {
+            "handlers": ["file", "stream"],  # Add the stream handler here
+            "level": "INFO",
+        }
+    }
+}
 
-    llm_chain = LLMChain(prompt=prompt, llm=bedrock_llm)
-    output = llm_chain.run({"doc_text": fulltext})
-    return output
 
+# Run the FastAPI app using uvicorn when the script is executed directly
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_config=logging_config)
--- app/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /home/ubuntu/gsa-rag/venv/bin/activate
-
-python3 /home/ubuntu/gsa-rag/main.py
